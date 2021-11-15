1d5982196484b726f65d4027374432760ab71835
Ivan Bevanda <86872168+BevandaIvan@users.noreply.github.com>
2021-11-15 13:14:47 +0100

    Update ValueNet.py

--- ValueNet.py
+++ ValueNet.py
@@ -5,9 +5,9 @@ import torch.optim as optim
 from tqdm import tqdm
 import numpy as np
 
-channels = 1
+channels = 4
 DIM = 64
-EPOCHS = 10
+EPOCHS = 50
 BATCH_SIZE = 8
 
 if torch.cuda.is_available():
@@ -19,8 +19,6 @@ class ValueNet(nn.Module):
 
     def __init__(self):
         super().__init__()
-
-        self.flattened_shape = None
 
         self.convs = nn.Sequential(
             nn.Conv1d(in_channels=channels, out_channels=DIM, kernel_size=4, stride=2, padding=1),
@@ -37,16 +35,14 @@ class ValueNet(nn.Module):
         )
 
         self.tail = nn.Sequential(
-            nn.Linear(in_features=self.flattened_shape, out_features=1)
-            
+            nn.Linear(in_features=2560, out_features=1)   
         )
 
     def forward(self, x):
         x = self.convs(x)
-        x = torch.flatten(x)
-        self.flattened_shape = x.size(dim=1)
+        x = x.view(-1, 2560)
         x = self.tail(x)
-        return x
+        return torch.clamp(x, min=-1, max=1)
 
 class TrainModel():
     
@@ -56,46 +52,53 @@ class TrainModel():
         self.model = self.model.double()
         self.model.to(Device)
 
-        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-4)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-5)
         self.loss_function = nn.MSELoss()
 
     def train_model(self):
 
-        training_data_X = np.load("training_data_X.npy")
-        training_data_X = torch.tensor(training_data_X).double().to(Device)
-        training_data_Y = np.load("training_data_Y.npy")
-        training_data_Y = torch.tensor(training_data_Y).double().to(Device)
+        data_X = np.load("training_data_X.npy")
+        data_X = torch.tensor(data_X).double().to(Device)
+        data_Y = np.load("training_data_Y.npy")
+        data_Y = torch.tensor(data_Y).double().to(Device)
 
-        test_data_X = np.load("test_data_X.npy")
-        test_data_X = torch.tensor(test_data_X).double().to(Device)
-        test_data_Y = np.load("test_data_Y.npy")
-        test_data_Y = torch.tensor(test_data_Y).double().to(Device)
+        border_ind = int(0.9*len(data_X))
+
+        training_data_X = data_X[0:border_ind]
+        training_data_Y = data_Y[0:border_ind]
+
+        test_data_X = data_X[border_ind:len(data_X)]
+        test_data_Y = data_Y[border_ind:len(data_Y)]
 
         minloss = 1e10
         for epoch in range (1, EPOCHS+1):
+            print(epoch)
+            cumloss_train = 0
             for i in tqdm(range(0, len(training_data_X), BATCH_SIZE)):
-                BATCH_X = training_data_X[i:i+BATCH_SIZE]
-                BATCH_Y = training_data_Y[i:i+BATCH_SIZE]
+                leftover = min(i+BATCH_SIZE, len(training_data_X))
+                BATCH_X = training_data_X[i:leftover]
+                BATCH_Y = training_data_Y[i:leftover].view(-1, 1)
 
                 self.model.zero_grad()
                 output = self.model(BATCH_X)
                 loss = self.loss_function(output, BATCH_Y)
+                cumloss_train += loss
                 loss.backward()
                 self.optimizer.step()
 
-            cumloss = 0
+            cumloss_val = 0
             for i in range (0, len(test_data_Y)):
                 with torch.no_grad():
-                    X = test_data_X[i]
-                    Y = test_data_Y[i]
-                    
+                    X = test_data_X[i].view(-1, 4, 81)
+                    Y = test_data_Y[i].view(-1, 1)
                     output = self.model(X)
                     loss = self.loss_function(output, Y)
-                    cumloss += loss
-            if cumloss < minloss:
-                minloss = cumloss
+                    cumloss_val += loss
+            if cumloss_val < minloss:
+                minloss = cumloss_val
                 bestepoch = epoch
                 bestparams = self.model.state_dict()
+            print("epoch:", "training loss:", cumloss_train, "validation loss:", cumloss_val)
 
         print("Best epoch:", bestepoch)
         torch.save(bestparams, "state_dicts.pt")
