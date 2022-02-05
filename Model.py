264d6aa43b4494d6de47294591f1e7548dd9279c
Ivan Bevanda <86872168+BevandaIvan@users.noreply.github.com>
2022-02-05 13:23:02 +0100

    Translated comments into English

--- Model.py
+++ Model.py
@@ -1,11 +1,11 @@
 import random
 import math
 
-C_PARAMETER = math.sqrt(3) #Exploitation vs. exploration parametar UCB1
-GRIEF_PENALTY = 0.2 #Kaznjava poteze koji protivniku daju da zauzme lokalno polje
-GRIEF_PENALTY_MIDDLE = 0.2 #Poseban parametar za kaznjavanju poteza koji protivniku daju da zauzme sredisnje polje
+C_PARAMETER = math.sqrt(3) #Exploitation vs. Exploration hyperparamter in UCB1
+GRIEF_PENALTY = 0.2 #Penalizes moves that let the enemy take a local board
+GRIEF_PENALTY_MIDDLE = 0.2 #Seperately penalizes moves that let the enemy take the central local board
 
-move_keys = { #Za I/O
+move_keys = { #For I/O
             "A9":0, "B9":1, "C9":2, "A8":3, "B8":4, "C8":5, "A7":6, "B7":7, "C7":8,
             "D9":9, "E9":10, "F9":11, "D8":12, "E8":13, "F8":14, "D7":15, "E7":16, "F7":17,
             "G9":18, "H9":19, "I9":20, "G8":21, "H8":22, "I8":23, "G7":24, "H7":25, "I7":26,
@@ -17,9 +17,9 @@ move_keys = { #Za I/O
             "G3":72, "H3":73, "I3":74, "G2":75, "H2":76, "I2":77, "G1":78, "H1":79, "I1":80
         }
 
-move_keys_inv = {v: k for k, v in move_keys.items()} #Obrnuti dict od move_keys
+move_keys_inv = {v: k for k, v in move_keys.items()} #Inverse dict of move_keys
 
-def argmax(array): #Argmax funkcija, vraca indeks najvece vrijednosti liste
+def argmax(array): #Argmax function; returns the index of the largest value in the array
     maks = array[0]
     maksind = 0
     for i in range (len(array)):
@@ -28,51 +28,51 @@ def argmax(array): #Argmax funkcija, vraca indeks najvece vrijednosti liste
             maksind = i
     return maksind
 
-class Game(): #Igraca ploca i pravila igre
+class Game(): #Game board and game rules
 
     def __init__(self):
 
-        self.board = [0]*81 #Igraca ploca: -1 krizic, 1 kruzic, 0 prazno
-        self.won = [0]*9 #Koja lokalna polja su zauzeta/popunjena
+        self.board = [0]*81 #Game board: -1 cross, 1 circle, 0 empty
+        self.won = [0]*9 #Which local boards are taken/filled up
 
-    def deepcopy_self(self): #Vraca deepcopy sebe
+    def deepcopy_self(self): #Returns a deepcopy of itself
 
         ret = Game()
         ret.board = list(self.board)
         ret.won = list(self.won)
         return ret
 
-    def get_legal_moves(self, move): #Vraca listu svih mogucih poteza iz trenutne pozicije
+    def get_legal_moves(self, move): #Returns a list of all possible moves from the current board state
 
         ret = []
-        if self.won[move[0] % 9]: #Ako prosli potez salje na zautezo polje
+        if self.won[move[0] % 9]: #In case the previous moves "sends" the agent to a taken local board.
             for i in range (9):
                 if self.won[i]:
                     continue
                 for j in range (9):
                     if self.board[9*i + j] == 0:
                         ret.append([9*i+j, [-1, 1][move[1]==-1]])
-        else: #Provjerava samo polje na koji prosli potez salje
+        else: #Checks only the board that the previous move "sends" to
             x = move[0] % 9 * 9
             for i in range (x, x+9):
                 if self.board[i] == 0:
                     ret.append([i, [-1, 1][move[1]==-1]])
         return ret
 
-    def is_local_taken_(self, move): #Inplace; provjerava jesu li neka lokalna polja zauzeta i azurira self.won
+    def is_local_taken_(self, move): #Inplace; checks if any local boards are won and updates self.won
 
         x = move[0] // 9 * 9
-        for i in range (0, 7, 3): #Redovi
+        for i in range (0, 7, 3): #Rows
             if self.board[x+i] == self.board[x+i+1] == self.board[x+i+2] != 0:
                 self.won[move[0] // 9] = move[1]
-        for i in range (0, 3): #Stupci
+        for i in range (0, 3): #Columns
             if self.board[x+i] == self.board[x+i+3] == self.board[x+i+6] != 0:
                 self.won[move[0] // 9] = move[1]
-        if self.board[x] == self.board[x+4] == self.board[x+8] != 0: #Glavna dijagonala
+        if self.board[x] == self.board[x+4] == self.board[x+8] != 0: #Main diagonal
             self.won[move[0] // 9] = move[1]
-        elif self.board[x+2] == self.board[x+4] == self.board[x+6] != 0: #Sporedna dijagonala
+        elif self.board[x+2] == self.board[x+4] == self.board[x+6] != 0: #Secondary diagonal
             self.won[move[0] // 9] = move[1]
-        else: #Ako nitko nije dobio, provjerava je li polje popunjeno
+        else: #If noone won the local board, checks if it is fully filled up
             flag = 0
             for i in range (x, x+9):
                 if self.board[i]:
@@ -80,49 +80,49 @@ class Game(): #Igraca ploca i pravila igre
             if flag == 9:
                 self.won[move[0] // 9] = -2
     
-    def is_terminal(self): #Provjerava je li igra gotova 
+    def is_terminal(self): #Checks if the game is over 
         
         ret = None
-        for i in range (0, 7, 3): #Redovi
+        for i in range (0, 7, 3): #Rows
             if self.won[i] == self.won[i+1] == self.won[i+2] and self.won[i] != 0 and self.won[i] != -2:
                 ret = self.won[i]
                 return ret
-        for i in range (0, 3): #Stupci
+        for i in range (0, 3): #Columns
             if self.won[i] == self.won[i+3] == self.won[i+6] and self.won[i] != 0 and self.won[i] != -2:
                 ret = self.won[i]
                 return ret
-        if self.won[0] == self.won[4] == self.won[8] and self.won[0] != 0 and self.won[0] != -2: #Glavna dijagonala
+        if self.won[0] == self.won[4] == self.won[8] and self.won[0] != 0 and self.won[0] != -2: #Main diagonal
             ret = self.won[0]
             return ret
-        elif self.won[2] == self.won[4] == self.won[6] and self.won[2] != 0 and self.won[2] != -2: #Sporedna dijagonala
+        elif self.won[2] == self.won[4] == self.won[6] and self.won[2] != 0 and self.won[2] != -2: #Secondary diagonal
             ret = self.won[2]
             return ret
-        elif 0 not in self.won: #Sve popunjeno, a nitko nije dobio - nerjeseno
+        elif 0 not in self.won: #All local boards won/filled up, but noone won - tie
             ret = 0
             return ret
-        else: #Igra jos traje
+        else: #Game is not over
             ret = -2
         return ret
 
-    def make_move_(self, move): #Inplace; odigra zeljeni potez
+    def make_move_(self, move): #Inplace; makes the desired move
 
         self.board[move[0]] = move[1]
         self.is_local_taken_(move)
 
-class Node(): #Cvor u MCTS game tree-u
+class Node(): #Node in the MCTS game tree
 
     def __init__(self, state, ind, parent_move, parent):
 
-        self.state = state #Trenutno stanje
-        self.ind = ind #Indeks u stablu (stablo je lista)
+        self.state = state #Current state
+        self.ind = ind #Index in the tree (the tree is a list)
         self.parent_move = parent_move 
         self.parent = parent
-        self.children = [] #Indeksi djece
+        self.children = [] #The indices of children
         self.children_moves = self.state.get_legal_moves(self.parent_move)
         self.wins = 0
         self.visits = 0
 
-class MCTS(Game): #Monte Carlo Tree Search algoritam
+class MCTS(Game): #Monte Carlo Tree Search Algorithm
 
     def __init__(self):
 
@@ -133,13 +133,13 @@ class MCTS(Game): #Monte Carlo Tree Search algoritam
         ret = []
         for i in node.children:
             temp = self.tree[i]
-            if temp.visits == 0: #Ako nije posjeceno, vrijednost je inf
+            if temp.visits == 0: #If it isn't visited, the value is inf
                 ret.append(123456)
                 continue
             ret.append(temp.wins / temp.visits + c*(math.sqrt(node.visits)/temp.visits))
         return ret
 
-    def selection(self, node): #"Spusta" se po stablu prema UCB1 do najboljeg postojeceg cvora
+    def selection(self, node): #"Descends" down the tree according to UCB1 to the best node
 
         while True:
             if len(node.children_moves):
@@ -151,7 +151,7 @@ class MCTS(Game): #Monte Carlo Tree Search algoritam
             break
         return node
 
-    def expansion_(self, node): #Inplace; prosiruje cvor za jedno dijete
+    def expansion_(self, node): #Inplace; expands the node by one child
 
         if node.state.is_terminal() != -2:
             return False
@@ -163,7 +163,7 @@ class MCTS(Game): #Monte Carlo Tree Search algoritam
             self.tree.append(child)
             node.children.append(child.ind)
 
-    def simulation(self, node): #Simulira nasumicne poteze do kraja igre iz nekog stanja i vraca rezultat (ne biljezi se)
+    def simulation(self, node): #Simulates random moves until the end of the game and returns the result (simulated game isn't recorded anywhere)
 
         board = node.state.deepcopy_self()
         move = [node.parent_move[0], node.parent_move[1]]
@@ -176,41 +176,41 @@ class MCTS(Game): #Monte Carlo Tree Search algoritam
             board.make_move_(move)
         return board.is_terminal()
 
-    def backpropagation_(self, node, result): #Inplace; podesava vrijednosti stabla rezultatom simulacije
+    def backpropagation_(self, node, result): #Inplace; modifies the values of the nodes with respect to the result of the simulation
 
-        if result == 1: #Dobio je kruzic (Model)
+        if result == 1: #Circle (Model) won
             node.wins += 1
-        elif result == 0: #Nerjeseno je
+        elif result == 0: #Tie
             node.wins += 0.5
         node.visits += 1
         if node.parent:
             self.backpropagation_(self.tree[node.parent], result)
 
-    def punish_grief_(self, weights, node): #Kaznjava lose poteze, implentirano rucno jer je model "darivao" polja
+    def punish_grief_(self, weights, node): #Punishes bad moves (check README)
 
         for i in range(len(node.children)):
             child = self.tree[node.children[i]]
-            current = child.state.won.count(-1) #Trenutni broj lokalnih polja koja je protivnik osvojio
+            current = child.state.won.count(-1) #Current number of local boards the enemy has
             for gc_ind in child.children:
                 grandchild = self.tree[gc_ind]
-                if grandchild.state.is_terminal() == -1: #Ako protivnik moze pobijediti sljedeci potez, nemoj odigrati
+                if grandchild.state.is_terminal() == -1: #If the enemy can win next move, don't play this
                     weights[i] = 0
                     break
-                if grandchild.state.won[4] == -1 and child.state.won[4] != -1: #Kaznjava potez koji protivniku daje da uzme sredisnje polje
+                if grandchild.state.won[4] == -1 and child.state.won[4] != -1: #Punishes a move that lets the enemy take the center local board
                     weights[i] -= GRIEF_PENALTY_MIDDLE*weights[i]
                     break
                 future = grandchild.state.won.count(-1) 
-                if future > current: #Kaznjava potez koji protivniku daju da uzme neko lokalno polje
+                if future > current: #Punishes a move that lets the enemy take some local board (not center)
                     weights[i] -= GRIEF_PENALTY*weights[i]
                     break
 
-    def search(self, state, move, difficulty): #Nalazi najbolji potez iz nekog stanja - MCTS algoritam
+    def search(self, state, move, difficulty): #Finds the best move from some game state - MCTS algorithm
 
         self.tree = []
-        self.tree.append(0) #Filler; Izvorni cvor mora biti na indeksu 1 zbog "if parent" u backpropagation_
+        self.tree.append(0) #Filler; Root node must be at index 1 because of "if parent" in backpropagation_
         first_node = Node(state, 1, move, 0)
         self.tree.append(first_node)
-        for diff in range (difficulty): #Broj iteracija MCTS algoritma, vise iteracija -> bolji model
+        for diff in range (difficulty): #More iterations -> better model
             node = self.selection(self.tree[1])
             self.expansion_(node)
             if node.children != []:
@@ -218,9 +218,9 @@ class MCTS(Game): #Monte Carlo Tree Search algoritam
             result = self.simulation(node)
             self.backpropagation_(node, result)
         node = self.tree[1]
-        weights = [self.tree[x].visits for x in node.children] #Bira najvise posjeceno dijete
+        weights = [self.tree[x].visits for x in node.children] #Selects the most visited child
         self.punish_grief_(weights, node)
-        for i in range(len(weights)): #Ispisuje koliko je svaki potez "dobar"
+        for i in range(len(weights)): #Prints how "good" each move is
             print(move_keys_inv[self.tree[node.children[i]].parent_move[0]], weights[i])
         move = self.tree[node.children[argmax(weights)]].parent_move
         return move
@@ -232,12 +232,12 @@ class Run(MCTS):
         
         self.game = Game()
 
-    def inputmove(self, prev_player): #Pretvara npr. A8 u reprezentaciju poteza u programu tj. [3, 1] (indeks, igrac)
+    def inputmove(self, prev_player): #Turns e.g. A8 into a representation of the move in the program [3, 1] (index, player)
 
         ret = [move_keys[input()], [-1, 1][prev_player==-1]]
         return ret
 
-    def play_game(self): #Igranje igre
+    def play_game(self): #Game playing
 
         difficulty = int(input())
         prev_player = 1
