8a0c6a643e54b3581e83fe53b44d0721e1e6ebb8
bornagojsic <69301624+bornagojsic@users.noreply.github.com>
2024-01-22 23:02:48 +0100

    Added first move reply lookup

--- flask_server/flaskapi.py
+++ flask_server/flaskapi.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 from main import PlayVsAI
 from constants import *
+import json
 import os
 
 app = Flask(__name__)
@@ -13,13 +14,13 @@ is_over = None
 game_ids = []
 games = {}
 ## hardcodeano
-num_iters=1000
+#num_iters=1000
 num_sims=1
 
 
 def clear_games():
     global games, game_ids
-    if len(games) > 10:
+    if len(games) > 50:
         del games[game_ids[0]]
         game_ids = game_ids[1:]
 
@@ -30,6 +31,9 @@ def handle_move():
     if request.method == 'POST':
         game_id = request.json['ID']
         move = request.json['move']
+
+        num_iters = int(request.json['numIters'])
+
         print(game_id)
         print(move)
 
@@ -37,6 +41,16 @@ def handle_move():
             game_ids.append(game_id)
             games[game_id] = {"game_run": PlayVsAI()}
             clear_games()
+            first_replys = open("first_replys.json", "r")
+            replys = json.load(first_replys)
+            first_replys.close()
+            try:
+                reply = replys[move]
+                reply_move = reply[0]
+                game_run = games[game_id]["game_run"]
+                return {'move': reply_move, 'boardWinners': game_run.game.won, 'isOver': False, 'numItersRecieved': num_iters}
+            except Exception:
+                pass
 
         game_run = games[game_id]["game_run"]
         move = game_run.input_move("x", move)
@@ -47,7 +61,7 @@ def handle_move():
         if winner != "-":
             return {'move': '', 'boardWinners': game_run.game.won, 'isOver': winner}
 
-        global move_iters, num_sims, hide_evaluations
+        global num_sims, hide_evaluations
         move = game_run.search(game_run.game, move, num_iters, 1, hide_evaluations=True)
         print(move_keys_inv[move[0]])
         game_run.make_move_(game_run.game, move)
@@ -57,7 +71,7 @@ def handle_move():
         winner = game_run.is_terminal(game_run.game)
         is_over = winner if winner != '-' else 'false'
 
-        return {'move': move_keys_inv[ai_move[0]], 'boardWinners': game_run.game.won, 'isOver': is_over}
+        return {'move': move_keys_inv[ai_move[0]], 'boardWinners': game_run.game.won, 'isOver': is_over, 'numItersRecieved': num_iters}
         # return {'message': 'move received'}
     elif request.method == 'GET':
         print("\nMOVE:", ai_move)
